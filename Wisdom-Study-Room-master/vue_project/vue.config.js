f8f8078ac62578a890837b0b88272d4e68ee06cd
Mainz <CzYi0412@gmail.com>
2025-12-23 16:57:59 +0800

    更新 API 代理目标地址为 http://1.94.66.156:8080

--- Wisdom-Study-Room-master/vue_project/vue.config.js
+++ Wisdom-Study-Room-master/vue_project/vue.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
       '/api': {
         // 使用环境变量 VUE_APP_API_URL（在 docker-compose 中设为 http://backend:8080），
         // 在本地开发时可不设置则回退到 localhost:8080
-        target: process.env.VUE_APP_API_URL || 'http://localhost:8080',
+        target: process.env.VUE_APP_API_URL || 'http://1.94.66.156:8080',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
